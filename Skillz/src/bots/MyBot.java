4f6ee3f4fd33fb0ac6a128e6fb114643fa9f6c1c
Itamar Rivkind <Michorn313@gmail.com>
2018-02-06 11:36:09 +0200

    Killed tryPush

--- Skillz/src/bots/MyBot.java
+++ Skillz/src/bots/MyBot.java
@@ -41,50 +41,5 @@ public class MyBot implements PirateBot {
 		// Try to push, if you didn't - take the capsule and go to the mothership.
 	}
 
-	/**
-	 * Makes the pirate try to push an enemy pirate or an asteroid. Returns True if
-	 * it did.
-	 *
-	 * @param pirate
-	 *            - the pushing pirate
-	 * @param game
-	 *            - the current game state
-	 * @return - true if the pirate pushed
-	 */
-	private boolean tryPush(Pirate pirate, PirateGame game) {
-		// Go over all enemies
-		for (Pirate enemy : game.getEnemyLivingPirates()) {
-			// Check if the pirate can push the enemy
-			if (pirate.canPush(enemy)) {
-				// Push enemy!
-				pirate.push(enemy, enemy.initialLocation);
-
-				// Print a message
-				System.out.println("pirate " + pirate + " pushes " + enemy + " towards " + enemy.initialLocation);
-
-				// Did push
-				return true;
-			}
-		}
-
-		// Go over all living asteroids
-		for (Asteroid asteroid : game.getLivingAsteroids()) {
-			// Check if the pirate can push the asteroid
-			if (pirate.canPush(asteroid)) {
-				// Push asteroid!
-				pirate.push(asteroid, game.getEnemyCapsules()[0]);
-
-				// Print a message
-				System.out
-						.println("pirate " + pirate + " pushes " + asteroid + " towards " + game.getEnemyCapsules()[0]);
-
-				// Did push
-				return true;
-			}
-		}
-
-		// Didn't push
-		return false;
-	}
-
+	
 }
